57c422fe967a82435a4508367eaf758e659b2ec2
hkustsungod-coder <ytangax@gmail.com>
2021-01-25 14:28:28 +0800

    P1

---
+++ app/src/main/java/com/example/biji/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.biji;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_layout);
+    }
+}
